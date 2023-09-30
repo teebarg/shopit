58a62a0cd4b98183e2e57d0ce6fd3f36fb9cbf6e
Adeniyi <teebarg01@gmail.com>
2023-09-30 13:49:04 +0100

    flake

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app.include_router(
 )  # Include the user router
 
 
-# Root path |||perrerseeoppo
+# Root path |||perrerseeoppopo
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
