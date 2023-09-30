ff471fc59a97db4e348c1c1cdc159774c13c95d5
Adeniyi <teebarg01@gmail.com>
2023-09-30 14:11:00 +0100

    flake

--- backend/main.py
+++ backend/main.py
@@ -11,15 +11,19 @@ app = FastAPI(title=settings.PROJECT_NAME)
 
 # Mount the routers under their respective paths
 app.include_router(products_router, prefix="/products", tags=["products"])
-app.include_router(collections_router, prefix="/collections", tags=["collections"])
+app.include_router(
+    collections_router, prefix="/collections", tags=["collections"]
+)
 app.include_router(cart_router, prefix="/cart", tags=["cart"])
-app.include_router(users_router, prefix="/users", tags=["users"])  # Include the user router
+app.include_router(
+    users_router, prefix="/users", tags=["users"]
+)  # Include the user router
 app.include_router(
     auth_router, prefix="/auth", tags=["auth"]
 )  # Include the user router
 
 
-# Root path |||perrerseeoppopopo
+# Root path
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
