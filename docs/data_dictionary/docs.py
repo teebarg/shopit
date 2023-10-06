a80a75ff6368444dcf9ebb21ac2c5883249ec80a
Adeniyi <teebarg01@gmail.com>
2023-10-06 14:21:48 +0100

    test github

--- docs/data_dictionary/docs.py
+++ docs/data_dictionary/docs.py
@@ -146,7 +146,7 @@ print(os.getcwd())
 print('Updating wiki file...222222')
 # List files in current directory
 print(os.listdir())
-print('Updating wiki file...33333')
+print('Updating wiki file...333333')
 # wiki_filepath = "./wiki.md"
 wiki_filepath = 'docs/data_dictionary/wiki.md'
 base_filepath = 'docs/data_dictionary'
