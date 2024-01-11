628725aa3da41eeec7973c05eb9deb37223928de
Adeniyi <teebarg01@gmail.com>
2024-01-11 17:29:39 +0100

    fix post call

--- frontend/lib/http.ts
+++ frontend/lib/http.ts
@@ -16,8 +16,7 @@ const Http = async (url: string, method: Method, body?: any, extra?: Object) => 
     return await fetch(process.env.NEXT_PUBLIC_API_DOMAIN + url, {
         method,
         headers: {
-            accept: "application/json",
-            "Content-Type": "application/json",
+            Accept: "application/json",
             Authorization: `Bearer ${session?.user?.accessToken}`,
         },
         ...extra,
